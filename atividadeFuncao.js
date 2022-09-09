8bf19bc60eb08634f9a3a81a1cd30edb979a1d9a
icarorc <108996608+icarorc@users.noreply.github.com>
2022-09-09 18:07:19 -0300

    Add files via upload

---
+++ atividadeFuncao.js
@@ -0,0 +1,16 @@
+function somar() {
+    let num1 = 5
+    let num2 = 5
+    return `O resultado da soma foi ${num1 + num2}`
+}
+console.log(somar())
+
+function dividir(num1,num2) {
+    return `O resultado da divisão é ${num1/num2}` 
+}
+
+console.log(dividir(10,2))
+
+const mult = (num1,num2) => `O resultado da multiplicação é ${num1 * num2} `
+
+console.log(mult(5,5))
